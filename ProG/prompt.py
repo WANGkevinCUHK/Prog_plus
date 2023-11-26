3a90d88560d736f72d6c3491d615701f3fbfe4c2
Barristen <65346840+Barristen@users.noreply.github.com>
2023-11-26 20:00:20 +0800

    Update prompt.py

--- ProG/prompt.py
+++ ProG/prompt.py
@@ -230,8 +230,8 @@ class GPF(torch.nn.Module):
 class GPF_plus(torch.nn.Module):
     def __init__(self, in_channels: int, p_num: int):
         super(GPF_plus, self).__init__()
-        self.p_list = nn.Parameter(torch.Tensor(p_num, in_channels))
-        self.a = nn.Linear(in_channels, p_num)
+        self.p_list = torch.nn.Parameter(torch.Tensor(p_num, in_channels))
+        self.a = torch.nn.Linear(in_channels, p_num)
         self.reset_parameters()
 
     def reset_parameters(self):
