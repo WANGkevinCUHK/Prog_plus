dba0ed091bd958bcde8bf9c934bf4397b1033587
Barristen <2246398498@qq.com>
2023-12-28 08:33:23 +0000

    test gppt prompt

--- node_task.py
+++ node_task.py
@@ -1,72 +1,71 @@
 
-from torch_geometric.datasets import Planetoid
-from torch_geometric.transforms import NormalizeFeatures
-from ProG.Model.model import GNN
+from ProG.Model.model import GNN, GPPT
 import torch
 from ProG.prompt import GPF,GPF_plus
-
-dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
-
-print()
-print(f'Dataset: {dataset}:')
-print('======================')
-print(f'Number of graphs: {len(dataset)}')
-print(f'Number of features: {dataset.num_features}')
-print(f'Number of classes: {dataset.num_classes}')
-
-data = dataset[0]  # Get the first graph object.
-
-print()
-print(data)
-print('===========================================================================================================')
-
-# Gather some statistics about the graph.
-print(f'Number of nodes: {data.num_nodes}')
-print(f'Number of edges: {data.num_edges}')
-print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
-print(f'Number of training nodes: {data.train_mask.sum()}')
-print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
-print(f'Has isolated nodes: {data.has_isolated_nodes()}')
-print(f'Has self-loops: {data.has_self_loops()}')
-print(f'Is undirected: {data.is_undirected()}')
-
+from ProG.Data.data import load_node_task
+from ProG.utils import constraint
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = GNN(input_dim=dataset.num_features,out_dim=dataset.num_classes, gnn_type='GCN')
-model = model.to(device)
+dataset_name ='Cora'
+data, dataset = load_node_task(dataset_name)
+data = data.to(device)
+data.train_id = torch.nonzero(data.train_mask).squeeze(1)
+model = GNN(input_dim=dataset.num_features,out_dim=dataset.num_classes, gnn_type='GCN').to(device)
 
 # setting prompt
-prompt_type = 'gpf-plus'
+prompt_type = 'gppt'
 if prompt_type == 'gpf':
       prompt = GPF(data.num_features).to(device)
 elif prompt_type == 'gpf-plus':
       prompt = GPF_plus(data.num_features,data.num_nodes ).to(device)
+elif prompt_type == 'gppt':
+      model = GPPT(in_feats = dataset.num_features, n_classes= dataset.num_classes).to(device)
+      model.weigth_init(data.x,data.edge_index,data.y,data.train_id)
+else:
+      prompt = None
 
-      
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 criterion = torch.nn.CrossEntropyLoss()
 
-def train():
-      model.train()
-      optimizer.zero_grad()  # Clear gradients.
-      out = model(data.x, data.edge_index, batch=None, prompt = prompt)  # Perform a single forward pass.
-      loss = criterion(out[data.train_mask], data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
-      loss.backward()  # Derive gradients.
-      optimizer.step()  # Update parameters based on gradients.
-      return loss
 
-def test(mask):
+def train(model, data):
+      # model.weigth_init(data.x,data.edge_index,data.y,data.train_id)
+      if prompt_type != 'gppt':
+            model.train()
+            optimizer.zero_grad() 
+            out = model(data.x, data.edge_index, batch=None, prompt = prompt) 
+            loss = criterion(out[data.train_mask], data.y[data.train_mask])  
+            loss.backward()  
+            optimizer.step()  
+            return loss
+      else:
+            model.train()
+            out = model(data.x, data.edge_index)
+            loss = criterion(out[data.train_mask], data.y[data.train_mask])
+            loss = loss + 0.001 * constraint(device,model.get_mul_prompt())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            model.update_prompt_weight(model.get_mid_h())
+            return loss
+
+      
+
+def test(model, data, mask):
       model.eval()
-      out = model(data.x, data.edge_index, batch=None, prompt = prompt)
-      pred = out.argmax(dim=1)  # Use the class with highest probability.
-      correct = pred[mask] == data.y[mask]  # Check against ground-truth labels.
-      acc = int(correct.sum()) / int(mask.sum())  # Derive ratio of correct predictions.
+      if prompt_type != 'gppt':
+            out = model(data.x, data.edge_index, batch=None, prompt = prompt)
+      else:
+            out = model(data.x, data.edge_index)
+      pred = out.argmax(dim=1)  
+      correct = pred[mask] == data.y[mask]  
+      acc = int(correct.sum()) / int(mask.sum())  
       return acc
 
 
-for epoch in range(1, 201):
+for epoch in range(1, 180):
     data = data.to(device)
-    loss = train()
-    val_acc = test(data.val_mask)
-    test_acc = test(data.test_mask)
-    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Val: {val_acc:.4f}, Test: {test_acc:.4f}')
+    loss = train(model,data)
+    val_acc = test(model,data,data.val_mask)
+    test_acc = test(model,data,data.test_mask)
+    print("Epoch {:03d} | Loss {:.4f} | val Accuracy {:.4f} | test Accuracy {:.4f} ".format(epoch, loss.item(), val_acc, test_acc))
