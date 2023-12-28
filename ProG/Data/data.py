60a4b18afa499d2d3cc9435e2c1ed45d2c6633b1
Barristen <2246398498@qq.com>
2023-12-28 08:33:45 +0000

    add load different load_data

--- ProG/Data/data.py
+++ ProG/Data/data.py
@@ -2,10 +2,11 @@ import torch
 import pickle as pk
 from random import shuffle
 
+from torch_geometric.datasets import Planetoid
 from torch_geometric.data import Batch
 from collections import defaultdict
 from torch_geometric.datasets import TUDataset
-
+from torch_geometric.transforms import NormalizeFeatures
 def multi_class_NIG(dataname, num_class,shots=100):
     """
     NIG: node induced graphs
@@ -58,7 +59,7 @@ def multi_class_NIG(dataname, num_class,shots=100):
 
     return train_data, test_data, train_list,test_list
 
-def load4graph(dataset_name):
+def load_graph_task(dataset_name):
 
     dataset = TUDataset(root='data/TUDataset', name=(dataset_name))
 
@@ -93,7 +94,35 @@ def load4graph(dataset_name):
     print(f'Number of training graphs: {len(train_dataset)}')
     print(f'Number of test graphs: {len(test_dataset)}')
      
-    return dataset,train_dataset,test_dataset 
+    return dataset, train_dataset, test_dataset 
+
+def load_node_task(dataname):
+    dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
+
+    print()
+    print(f'Dataset: {dataset}:')
+    print('======================')
+    print(f'Number of graphs: {len(dataset)}')
+    print(f'Number of features: {dataset.num_features}')
+    print(f'Number of classes: {dataset.num_classes}')
+
+    data = dataset[0]  # Get the first graph object.
+
+    print()
+    print(data)
+    print('===========================================================================================================')
+
+    # Gather some statistics about the graph.
+    print(f'Number of nodes: {data.num_nodes}')
+    print(f'Number of edges: {data.num_edges}')
+    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    print(f'Number of training nodes: {data.train_mask.sum()}')
+    print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+    print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+    print(f'Has self-loops: {data.has_self_loops()}')
+    print(f'Is undirected: {data.is_undirected()}')
+
+    return data,dataset
 
 if __name__ == '__main__':
     pass
